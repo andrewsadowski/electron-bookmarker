7e99af645f577bc82409f190f8edc6555dbcac92
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-03-18 13:26:18 -0700

    addToPage function added

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,8 @@
 const newLinkUrl = document.querySelector('#new-link-url');
 const newLinkSubmit = document.querySelector('.new-link-form--submit');
 const newLinkForm = document.querySelector('.new-link-form');
+const linkTemplate = document.querySelector('#link-template');
+const linksSection = document.querySelector('.links');
 
 newLinkUrl.addEventListener('keyup', () => {
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
@@ -10,6 +12,19 @@ const parser = new DOMParser();
 const parseResponse = text => parser.parseFromString(text, 'text/html');
 const findTitle = nodes => nodes.querySelector('title').textContent;
 
+const addToPage = ({ title, url }) => {
+  const newLink = linkTemplate.content.cloneNode(true);
+  const titleElement = newLink.querySelector('.link--title');
+  const urlElement = newLink.querySelector('.link--url');
+
+  titleElement.textContent = title;
+  urlElement.href = url;
+  urlElement.textContent = url;
+
+  linksSection.appendChild(newLink);
+  return { title, url };
+};
+
 newLinkForm.addEventListener('submit', () => {
   event.preventDefault();
 
@@ -18,6 +33,8 @@ newLinkForm.addEventListener('submit', () => {
     .then(response => response.text())
     .then(parseResponse)
     .then(findTitle)
+    .then(title => ({ title, url }))
+    .then(addToPage)
     .then(title => console.log(title))
     .catch(error => console.error(error));
 });
