6988dee078483fdc8d0f70cad89afd031d0da4cf
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-03-18 12:37:40 -0700

    added mainWindow to globale

--- app/main.js
+++ app/main.js
@@ -1,11 +1,21 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow = null;
+
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
     width: 300,
-    height: 600
+    height: 600,
+    show: false
   });
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
-
-
