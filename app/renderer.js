6b6038ce740a67aaf47036375940f845b927c3db
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-03-18 12:53:48 -0700

    added newLinkUrl keyup eventlistener

---
+++ app/renderer.js
@@ -0,0 +1,19 @@
+const newLinkUrl = document.querySelector('#new-link-url');
+const newLinkSubmit = document.querySelector('.new-link-form--submit');
+
+newLinkUrl.addEventListener('keyup', () => {
+  newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+
+// const os = require('os');
+// const fs = require('fs');
+
+// const files = fs.readdirSync(os.homedir());
+
+// console.log(files);
+
+// files.forEach(name => {
+//   const file = document.createElement('li');
+//   file.textContent = name;
+//   document.body.appendChild(file);
+// });
